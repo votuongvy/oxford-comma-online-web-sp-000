8fa57f3ded95e6d82c924b7e6c85c319341d2fd5
vy vo <tuonggvy.vo@gmail.com>
2019-11-01 04:34:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,5 +5,7 @@ def oxford_comma(array)
     array.join(" and ")
   elsif array.size == 3
     return array[0,1].join(", ") + ", and "+ array[2]
+  else
+    return array[0..-2].join(", ") + ", and " + array[-1]
   end 
 end
