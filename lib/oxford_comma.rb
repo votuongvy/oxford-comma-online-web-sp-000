121f0c6c388f4423848dd8095111f2de9ebea7bb
vy vo <tuonggvy.vo@gmail.com>
2019-11-01 04:35:23 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array.join
+  elsif array.size == 2
+    array.join(" and ")
+  elsif array.size == 3
+    return array[0..1].join(", ") + ", and "+ array[2]
+  else
+    return array[0..-2].join(", ") + ", and " + array[-1]
+  end 
 end
