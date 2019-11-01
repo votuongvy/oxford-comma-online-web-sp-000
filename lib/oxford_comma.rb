7e6fbb7002d90465f5604112a076edb636b8eb69
vy vo <tuonggvy.vo@gmail.com>
2019-11-01 04:35:05 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.size == 2
     array.join(" and ")
   elsif array.size == 3
-    return array[0,1].join(", ") + ", and "+ array[2]
+    return array[0..1].join(", ") + ", and "+ array[2]
   else
     return array[0..-2].join(", ") + ", and " + array[-1]
   end 
